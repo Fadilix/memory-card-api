67a427d19f00ceb17a558e2fe506cc480ebc203f
Fadilix <iamfadile2@gmail.com>
2024-10-08 11:18:41 +0000

    feat: made the game played and the bestcore optional

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,23 +74,24 @@ const updateBestScoreAndGamePlayed = async (req, res) => {
     try {
         const { id } = req.params;
         const { bestScore, gamePlayed } = req.body;
-        const bestScoreInt = parseInt(bestScore);
-        const gamePlayedInt = parseInt(gamePlayed);
-        
-        const currentUser = await prisma.user.findUnique({
-            where: { id },
-            select: { gamePlayed: true }
-        });
+
+        const updateData = {};
+        if (bestScore !== undefined) {
+            updateData.bestScore = parseInt(bestScore);
+        }
+        if (gamePlayed !== undefined) {
+            const currentUser = await prisma.user.findUnique({
+                where: { id },
+                select: { gamePlayed: true }
+            });
+            updateData.gamePlayed = currentUser.gamePlayed + parseInt(gamePlayed);
+        }
 
         const updatedUser = await prisma.user.update({
-            where: {
-                id
-            },
-            data: {
-                bestScore: bestScoreInt,
-                gamePlayed: currentUser.gamePlayed + gamePlayedInt 
-            }
-        })
+            where: { id },
+            data: updateData
+        });
+
         res.status(200).json({ message: "User updated successfully", user: updatedUser });
     } catch (error) {
         res.status(500).json({ message: "Internal server error", error: error.message });
